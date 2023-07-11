bac9726c906c4c8997d833d2973f063d4b9d21cd
Thélio Freitas <marquesthelio@hotmail.com>
2023-07-10 21:08:56 -0300

    Combobox to select API source in LWC

--- force-app/main/default/lwc/bookImporter/bookImporter.js
+++ force-app/main/default/lwc/bookImporter/bookImporter.js
@@ -3,16 +3,29 @@ import searchBooks from '@salesforce/apex/BookImporterController.searchBooks';
 
 export default class BookImporter extends LightningElement {
     @api searchTerm = '';
+    @api source = '';
     searchResults;
     startIndex = 0;
     maxResults = 15;
 
+    get sourceOptions() {
+        return [
+            { label: 'Google Books', value: 'GoogleBookAPI' },
+            { label: 'Open Library', value: 'OpenLibraryAPI' },
+            { label: 'Marvel', value: 'MarvelAPI' },
+        ];
+    }
+    
     handleSearchTermChange(event) {
         this.searchTerm = event.target.value;
     }
 
+    handleSourceChange(event) {
+        this.source = event.target.value;
+    }
+
     handleSearch() {
-        searchBooks({ searchTerm: this.searchTerm, startIndex: this.startIndex, maxResults: this.maxResults, api: 'MarvelAPI' })
+        searchBooks({ searchTerm: this.searchTerm, startIndex: this.startIndex, maxResults: this.maxResults, api: this.source })
             .then(response => {
                 this.searchResults = response.results;
                 console.log(response.totalResults);
